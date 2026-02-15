09e0d188fd50a769c6db2c0529d35f8c144bcbb0
pradashades333 <alexdreve03@gmail.com>
2026-02-15 01:31:00 +0100

    added buttons to change the pages will add css later maybe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import createHome from "./home.js";
+import createContact from "./contact.js";
+import createMenu from "./menu.js";
 
 createHome();
+
+
+const home = document.getElementById("home-button")
+const contact = document.getElementById("contact-button")
+const menu = document.getElementById("menu-button")
+
+home.addEventListener("click", () => {
+    createHome();
+})
+
+menu.addEventListener("click", () => {
+    createMenu();
+})
+
+contact.addEventListener("click", () => {
+    createContact();
+})
